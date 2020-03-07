38b53e2158aec9d0b85ef37efb1e53b4ba919b4d
n8ta <n8@u.northwestern.edu>
2020-03-07 10:14:22 -0600

    output predictions

--- predict_and_grapy.py
+++ predict_and_grapy.py
@@ -8,19 +8,11 @@ import os
 import pickle as pkl
 
 input_data = "skype"
-source_data = "full"
-
-
+trace_types = ['filezilla', 'spotifyOnline', 'VLC', 'skype', 'spotifyOffline', 'winscp', 'winrar']
 
 print("started loading saved model")
 model = tensorflow.saved_model.load('trained')
 print("finished loading model")
-
-#dict_keys(['spotifyOffline', 'winrar', 'VLC', 'spotifyOnline', 'winSCP', 'skype', 'filezilla'])
-
-
-
-
 
 Anew, Xnew, _, _, _, _ = prep(input_data)
 Anew_raw = Anew.toarray()  # array from of adj
@@ -29,12 +21,7 @@ Xnew = tensorflow.convert_to_tensor(Xnew, float)
 Anew = tensorflow.convert_to_tensor(Anew.toarray(), float)
 out = model([Xnew, Anew])
 
-trace_types = ['filezilla'
-               'spotifyOnline'
-               'VLC'
-               'skype'
-               'spotifyOffline'
-               'winscp'
-               'winrar']
 np = out.numpy()
+pkl.dump(np, open("prediction.pkl"), 'r+')
+
 condense_and_plot(Anew_raw, out.numpy().tolist(), trace_types)
